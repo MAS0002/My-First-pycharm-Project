7ce7adda5a0622496fe19d7a2900468fa6d11310
MAS0002 <mas.info0220@gmail.com>
2024-09-15 15:25:50 +0500

    Add files via upload

---
+++ First.py
@@ -0,0 +1,20 @@
+print ("Hello World")
+print("My Name is Muhammad Abdullah Shafique")
+
+#arithmetic operators
+a=4
+b=6
+print (a+b)
+print (a*b)
+print(a-b)
+print(a/b)
+print(10%3)
+
+print(5**2)  #5*5
+print(5**3)  #5*5*5
+print(10//3)  #floordevision
+
+x=10
+y=10
+print(x is y,x==y)
+print(x is not y,x!=y)
